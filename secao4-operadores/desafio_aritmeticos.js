acdace20753c7236643caa46cd85dc04557f16cd
awpadilha <padilha.a.w@gmail.com>
2020-10-26 13:54:30 -0300

    feat: Secao 4: Iniciando com Programacao

---
+++ secao4-operadores/desafio_aritmeticos.js
@@ -0,0 +1,18 @@
+let f = 73
+;
+
+//   Fórmula
+// F - 32     C
+// ------ = -----
+//   9        5
+console.log();
+/* tentativa
+a = 5 * f - 32
+b = 9
+c = a / b
+
+console.log(c);
+*/
+
+let c = (f - 32) / 9 * 5;
+console.log(c);
