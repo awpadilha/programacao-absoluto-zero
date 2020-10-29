e6ac0a2ff62e08971f0bd2d6c3ca095efad22787
awpadilha <padilha.a.w@gmail.com>
2020-10-29 18:26:03 -0300

    feat: Secao 4: Iniciando com Programacao

---
+++ secao4-operadores/testes-fixacao.js
@@ -0,0 +1,12 @@
+let texto = 'Olá';
+
+texto += 'oi';
+texto -= 'oi';
+texto *= 'oi';
+texto /= 'oi';
+texto %= 'oi';
+
+
+console.log(texto)
+
+// o unico oreperador aritmietico que funciona em uma atribuição de concatenção é o mais (+)
