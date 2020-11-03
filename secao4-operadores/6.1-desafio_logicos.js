bd86ac9d4e6e402013f3694ca440ba4379523ab7
awpadilha <padilha.a.w@gmail.com>
2020-11-03 16:12:49 -0300

    feat: Secao 4: Iniciando com Programacao

---
+++ secao4-operadores/6.1-desafio_logicos.js
@@ -0,0 +1,19 @@
+// Duas expressões resultar em TRUE ( Aritmeticos/ Relacioanais/ Lógicos)
+
+let verdadeira1 = 2 + 5 >= 7 && 8 > 2;
+let verdadeira2 = !(6 * 6 < 3);
+
+console.log(verdadeira1);
+console.log(verdadeira2);
+
+// Duas expressões resultar em FALSE ( Aritmeticos/ Relacioanais/ Lógicos)
+
+//let falso1 = 5 + 8 > 15 && 20 - 1 <= 2;
+//let falso2 = !( 5 + 5 >=10);
+
+let falso1 = 8 - 4 > 12 || false || 15 === '15';
+let falso2 = !(7 !== 6 + 1 || '13' !== 3);
+
+console.log(falso1);
+console.log(falso2);
+
