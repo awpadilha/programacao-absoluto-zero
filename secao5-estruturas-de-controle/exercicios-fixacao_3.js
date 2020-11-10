3642253b7e5846d047304d6ccf0e32ca70254125
awpadilha <padilha.a.w@gmail.com>
2020-11-10 20:01:04 -0300

    feat: Secao 5: Estruturas de Controle

--- secao5-estruturas-de-controle/exercicios-fixacao_3.js
+++ secao5-estruturas-de-controle/exercicios-fixacao_3.js
@@ -1,16 +1,18 @@
-const almoco = 20;
+const almoco = 25;
 let aviso;
 
 if (almoco < 13) {
     aviso = 'Já tá com fome, é?';
 } else if (almoco == 13) {
     aviso = 'Hora de encher a barriguinha!';
-} else if (almoco > 13) {
-    aviso = 'Perdeu o almoço playboy!';
-} else if(almoco == 19) {
+} else if (almoco > 13 && almoco <= 19) {
+    aviso = 'Perdeu o almoço, agora espera a janta!';
+} else if (almoco == 20) {
     aviso = 'Perdeu o almoço, mas garantiu a janta!';
-} else {
+} else if (almoco > 20 && almoco < 24) {
     aviso = 'Vai ter que pedir um iFood!';
+} else {
+    aviso = 'Dorme que passa!';
 }
 
 console.log(aviso);
