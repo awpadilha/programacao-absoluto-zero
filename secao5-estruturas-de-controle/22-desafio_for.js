7512054920ccb7989aedc50f6071e3fdabaef493
awpadilha <padilha.a.w@gmail.com>
2020-11-13 07:03:36 -0300

    feat: Secao 5: Estruturas de Controle

---
+++ secao5-estruturas-de-controle/22-desafio_for.js
@@ -0,0 +1,25 @@
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+
+// let linha = '';
+
+// for(let i = 1; i <= 6; i++) {
+//     linha += '#';
+//     console.log(linha);
+// }
+
+// console.log(linha);
+
+let linha = '';
+
+for(let i = ``; i <= `######`; i++) {
+    linha += '#';
+    console.log(linha);
+}
+
+console.log(linha);
+
