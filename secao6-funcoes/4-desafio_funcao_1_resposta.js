79c503728d239228c67131fff491a7f22a81c148
awpadilha <padilha.a.w@gmail.com>
2020-11-18 15:04:17 -0300

    feat: Secao 6: Funcoes

---
+++ secao6-funcoes/4-desafio_funcao_1_resposta.js
@@ -0,0 +1,22 @@
+// função: 2 params -> console.log(+)
+// função: 2 params -> console.log(-)
+
+// ...(3, 4)
+// ...(31, 98)
+
+// ...(11, 7)
+// ...(19,52)
+
+function somar(n1 , n2) {
+    console.log(n1 + n2);
+}
+
+function subtrair(a, b) {
+    console.log(a - b);
+}
+
+somar(3, 4);
+somar(31, 98);
+
+subtrair(11, 7);
+subtrair(19, 52);
