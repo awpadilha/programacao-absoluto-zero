79f97ef9e55c0676e313232ee5fff5fe917dbd0d
awpadilha <padilha.a.w@gmail.com>
2020-12-01 19:33:31 -0300

    feat: Secao 6: Funcoes

---
+++ secao6-funcoes/17-retornar_funcao_2.js
@@ -0,0 +1,13 @@
+ function retornarUmaFuncao() {
+     return function() {
+         return function(){
+             return "Boa tarde!!!"
+         }
+     }
+ }
+
+console.log(retornarUmaFuncao);
+console.log(retornarUmaFuncao());
+console.log(retornarUmaFuncao()());
+console.log(retornarUmaFuncao()()());
+ 
