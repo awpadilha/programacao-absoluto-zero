1dea025f3b0edca4fd71c9e581286a5ed38e0dee
awpadilha <padilha.a.w@gmail.com>
2020-12-08 18:13:55 -0300

    feat: Secao 7: Arrays

---
+++ secao7-arrays/3-basico_3.js
@@ -0,0 +1,17 @@
+console.log(typeof console);
+console.log(typeof console.log);
+
+const numeros = [1, 2, 3];
+
+console.log(typeof numeros);
+
+
+numeros.push(4);
+numeros.push(5);
+numeros.push(10.98);
+
+console.log(numeros);
+console.log(numeros.length);
+
+
+// funcao quee pertence ao um objeto pode ser chamada de método
