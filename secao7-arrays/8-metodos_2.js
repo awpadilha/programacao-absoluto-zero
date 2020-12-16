6a999f53683748e191976bc30f85df9cc9e935fd
awpadilha <padilha.a.w@gmail.com>
2020-12-16 13:57:50 -0300

    feat: Secao 7: Arrays

---
+++ secao7-arrays/8-metodos_2.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 3, 4, 5, 8];
+
+numeros.splice(1, 2); //retira numeros do array
+
+console.log(numeros);
+
+console.log(numeros.pop()); //remove o ultimo elemento do array, e também pode retornar-lo
+console.log(numeros.pop());
+console.log(numeros);
