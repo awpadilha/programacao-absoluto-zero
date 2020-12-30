8d9fc5a31ebac338e0924034bf93ee6255ef72b1
awpadilha <padilha.a.w@gmail.com>
2020-12-30 18:11:16 -0300

    feat: Secao 8: Objetos

---
+++ secao8-objetos/12-funcao_e_objeto_2.js
@@ -0,0 +1,18 @@
+function criardata(dia, mes, ano) {
+    return {
+        dia,
+        mes,
+        ano,
+        exibir() {
+            return `${this.dia}/${this.mes}/${this.ano}`;
+        }
+    };
+}
+
+const d1 = criardata(9, 10, 2010);
+const d2 = criardata(22, 11, 2023);
+const d3 = criardata(3, 4, 2022);
+
+console.log(d1.exibir());
+console.log(d2.exibir());
+console.log(d3.exibir());
---
+++ secao8-objetos/12-funcao_e_objeto_3.js
@@ -0,0 +1,9 @@
+// Função Construtora -> Objeto 
+function produto() {
+     console.log(this); 
+}
+
+const pq = new produto();
+
+console.log(typeof p1);
+
